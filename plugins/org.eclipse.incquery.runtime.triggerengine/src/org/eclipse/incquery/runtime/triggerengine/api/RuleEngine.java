9b19c3e613615251681377e0209147e091a4b5eb
abelhegedus <hegedusa@mit.bme.hu>
2013-01-21 17:05:41 +0100

    [398641] removing old classes  ASSIGNED - Consolidate callback functionality into trigger engine  https://bugs.eclipse.org/bugs/show_bug.cgi?id=398641

--- plugins/org.eclipse.incquery.runtime.triggerengine/src/org/eclipse/incquery/runtime/triggerengine/api/RuleEngine.java
+++ plugins/org.eclipse.incquery.runtime.triggerengine/src/org/eclipse/incquery/runtime/triggerengine/api/RuleEngine.java
@@ -1,92 +1,94 @@
 /*******************************************************************************
- * Copyright (c) 2010-2012, Tamas Szabo, Abel Hegedus, Istvan Rath and Daniel Varro
+ * Copyright (c) 2010-2013, Abel Hegedus, Istvan Rath and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Tamas Szabo, Abel Hegedus - initial API and implementation
+ *   Abel Hegedus - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.incquery.runtime.triggerengine.api;
 
-import java.lang.ref.WeakReference;
-import java.util.Map;
-import java.util.WeakHashMap;
+import static com.google.common.base.Preconditions.checkNotNull;
 
-import org.eclipse.emf.common.notify.Notifier;
-import org.eclipse.incquery.runtime.api.EngineManager;
+import java.util.Set;
+
+import org.eclipse.incquery.runtime.api.IPatternMatch;
 import org.eclipse.incquery.runtime.api.IncQueryEngine;
-import org.eclipse.incquery.runtime.exception.IncQueryException;
-import org.eclipse.incquery.runtime.triggerengine.specific.DefaultRuleFactory;
+import org.eclipse.incquery.runtime.api.IncQueryMatcher;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
 
 /**
- * The AbstractRule engine extends the functionality of EMF-IncQuery by providing the basic facilities to create
- * transformation rules. A transformation rule consists of the precondition being an EMF-IncQuery pattern and the
- * postcondition defined as a portion of an arbitrary Java code.
- * 
- * <p>
- * This class can be used to instantiate and lookup Agendas for a specific {@link Notifier} or {@link IncQueryEngine}
- * instance. The Agenda acts as an up-to-date collection of the fireable rule activations (similar to the term known
- * from the context of rule based expert systems).
- * 
- * @author Tamas Szabo
+ * @author Abel Hegedus
  * 
  */
 public class RuleEngine {
 
-    private static RuleEngine instance;
-    private Map<IncQueryEngine, WeakReference<IAgenda>> agendaMap;
-    private IRuleFactory defaultRuleFactory;
+    private Agenda agenda;
 
-    public static synchronized RuleEngine getInstance() {
-        if (instance == null) {
-            instance = new RuleEngine();
-        }
-        return instance;
+    protected RuleEngine(final Agenda agenda) {
+        this.agenda = checkNotNull(agenda, "Cannot create rule engine with null agenda!");
     }
 
-    protected RuleEngine() {
-        this.agendaMap = new WeakHashMap<IncQueryEngine, WeakReference<IAgenda>>();
-        this.defaultRuleFactory = new DefaultRuleFactory();
+    public static RuleEngine create(final Agenda agenda) {
+        return new RuleEngine(agenda);
     }
 
-    public IAgenda getOrCreateAgenda(Notifier notifier) {
-        return getOrCreateAgenda(notifier, false);
-    }
-
-    public IAgenda getOrCreateAgenda(Notifier notifier, boolean allowMultipleFiring) {
-        IncQueryEngine engine;
-        try {
-            engine = EngineManager.getInstance().getIncQueryEngine(notifier);
-            return getOrCreateAgenda(engine, allowMultipleFiring);
-        } catch (IncQueryException e) {
-            return null;
-        }
-    }
-
-    public IAgenda getOrCreateAgenda(IncQueryEngine engine) {
-        return getOrCreateAgenda(engine, false);
-    }
-
-    public IAgenda getOrCreateAgenda(IncQueryEngine engine, boolean allowMultipleFiring) {
-        IAgenda agenda = getAgenda(engine);
-        if (agenda == null) {
-            Agenda newAgenda = new Agenda(engine, allowMultipleFiring);
-            newAgenda.setRuleFactory(defaultRuleFactory);
-            agenda = newAgenda;
-            agendaMap.put(engine, new WeakReference<IAgenda>(agenda));
-        }
+    /**
+     * @return the agenda
+     */
+    protected Agenda getAgenda() {
         return agenda;
     }
 
-    private IAgenda getAgenda(IncQueryEngine iqEngine) {
-        WeakReference<IAgenda> agendaRef = agendaMap.get(iqEngine);
-        if (agendaRef != null) {
-            return agendaRef.get();
-        } else {
-            return null;
-        }
+    public IncQueryEngine getIncQueryEngine() {
+        return getAgenda().getIncQueryEngine();
+    }
+
+    public Multimap<ActivationState, Activation<?>> getActivations() {
+        return ImmutableMultimap.copyOf(agenda.getActivations());
+    }
+
+    public Set<Activation<?>> getActivations(final ActivationState state) {
+        checkNotNull(state, "Activation state must be specified!");
+        return ImmutableSet.copyOf(agenda.getActivations(state));
+    }
+
+    public <Match extends IPatternMatch, Matcher extends IncQueryMatcher<Match>> Set<Activation<Match>> getActivations(
+            final RuleSpecification<Match, Matcher> specification) {
+        checkNotNull(specification, "Rule specification must be specified!");
+        return ImmutableSet.copyOf(agenda.getActivations(specification));
+    }
+
+    public <Match extends IPatternMatch, Matcher extends IncQueryMatcher<Match>> Set<Activation<Match>> getActivations(
+            final RuleSpecification<Match, Matcher> specification, final ActivationState state) {
+        checkNotNull(specification, "Rule specification must be specified!");
+        checkNotNull(state, "Activation state must be specified!");
+        return ImmutableSet.copyOf(agenda.getInstance(specification).getActivations(state));
+    }
+
+    public <Match extends IPatternMatch, Matcher extends IncQueryMatcher<Match>> boolean addRule(
+            final RuleSpecification<Match, Matcher> specification) {
+        checkNotNull(specification, "Rule specification must be specified!");
+        RuleInstance<Match, Matcher> instance = agenda.instantiateRule(specification);
+        return instance != null;
+    }
+
+    public Set<RuleSpecification<? extends IPatternMatch, ? extends IncQueryMatcher<? extends IPatternMatch>>> getRules() {
+        return ImmutableSet.copyOf(agenda.getRuleInstanceMap().keySet());
+    }
+
+    public <Match extends IPatternMatch, Matcher extends IncQueryMatcher<Match>> boolean removeRule(
+            final RuleSpecification<Match, Matcher> specification) {
+        checkNotNull(specification, "Rule specification must be specified!");
+        return agenda.removeRule(specification);
+    }
+
+    public void dispose() {
+        agenda.dispose();
     }
 }
