428a9f704dc0f94f5f87296e9e3b62e24a56136b
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2013-02-01 13:28:32 +0100

    [398827] List Viewer support  NEW - Support query-based viewers  https://bugs.eclipse.org/bugs/show_bug.cgi?id=398827

---
+++ plugins/org.eclipse.incquery.viewers.runtime/src/org/eclipse/incquery/querybasedui/runtime/IncQueryViewerSupport.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2013, Zoltan Ujhelyi, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zoltan Ujhelyi - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.incquery.querybasedui.runtime;
+
+import org.eclipse.incquery.querybasedui.runtime.model.ViewerDataModel;
+import org.eclipse.incquery.querybasedui.runtime.sources.ListContentProvider;
+import org.eclipse.incquery.querybasedui.runtime.sources.QueryLabelProvider;
+import org.eclipse.jface.viewers.AbstractListViewer;
+
+/**
+ * @author Zoltan Ujhelyi
+ *
+ */
+public class IncQueryViewerSupport {
+
+    public static void bind(AbstractListViewer viewer, ViewerDataModel model) {
+        viewer.setContentProvider(new ListContentProvider());
+        viewer.setLabelProvider(new QueryLabelProvider());
+        viewer.setInput(model);
+    }
+}
