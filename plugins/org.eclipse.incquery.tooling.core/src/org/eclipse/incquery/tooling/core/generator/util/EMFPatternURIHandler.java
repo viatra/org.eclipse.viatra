f4708a4427276b83887f44e4298e3b7942f3eaad
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2013-02-10 23:36:09 +0100

    [399832] Replaced nsURI computation to work with nested packages  ASSIGNED - Wrong package nsURI is generated into the globalEiqModel.xmi when using packages from eiqgen  https://bugs.eclipse.org/bugs/show_bug.cgi?id=399832

--- plugins/org.eclipse.incquery.tooling.core/src/org/eclipse/incquery/tooling/core/generator/util/EMFPatternURIHandler.java
+++ plugins/org.eclipse.incquery.tooling.core/src/org/eclipse/incquery/tooling/core/generator/util/EMFPatternURIHandler.java
@@ -12,35 +12,63 @@
 package org.eclipse.incquery.tooling.core.generator.util;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.URIHandlerImpl;
+
+import com.google.common.collect.Maps;
 
 public class EMFPatternURIHandler extends URIHandlerImpl {
 
-    private final Map<URI, EPackage> uriToEPackageMap = new HashMap<URI, EPackage>();
+    private final Map<URI, EPackage> uriToEPackageMap = Maps.newHashMap();
+    private final Map<EPackage, String> packageFragmentMap = Maps.newHashMap();
+    private final Map<EPackage, String> packageUriMap = Maps.newHashMap();
 
     public EMFPatternURIHandler(Collection<EPackage> packages) {
         for (EPackage e : packages) {
-            if (e.eResource() != null) {
-                uriToEPackageMap.put(e.eResource().getURI(), e);
+            Resource resource = e.eResource();
+            if (resource != null) {
+                uriToEPackageMap.put(EcoreUtil.getURI(e), e);
+                packageFragmentMap.put(e, resource.getURIFragment(e));
+                packageUriMap.put(e, calculateModifiedUri(e, ""));
             }
         }
+    }
+
+    private String calculateModifiedUri(EPackage e, String fragment) {
+        String uri = e.getNsURI();
+        if (e.getESuperPackage() != null && uri.startsWith(e.getESuperPackage().getNsURI())) {
+            String newFragment = uri.substring(e.getESuperPackage().getNsURI().length()) + fragment;
+            return calculateModifiedUri(e.getESuperPackage(), newFragment);
+        }
+        return e.getNsURI() + "#/" + fragment;
+        // if (fragment != "" && fragment != null) {
+        // return fragment;
+        // }
+        // return res.getURIFragment(e);
     }
 
     @Override
     public URI deresolve(URI uri) {
         if (uri.isPlatform()) {
             String fragment = uri.fragment();
-            URI fragmentRemoved = uri.trimFragment();
-            EPackage p = uriToEPackageMap.get(fragmentRemoved);
-            if (p != null) {
-                URI newURI = URI.createURI(p.getNsURI());
-                newURI = newURI.appendFragment(fragment);
-                return newURI;
+            
+            String testFragment = fragment;
+            while (!testFragment.isEmpty()) {
+                int index = testFragment.lastIndexOf("/");
+                testFragment = testFragment.substring(0, index);
+                URI fragmentRemoved = uri.appendFragment(testFragment);
+                EPackage p = uriToEPackageMap.get(fragmentRemoved);
+                if (p != null) {
+                    URI newURI = URI.createURI(packageUriMap.get(p));
+                    String newFragment = newURI.fragment() + fragment.substring(index);
+                    newURI = newURI.appendFragment(newFragment);
+                    return newURI;
+                }
             }
         }
         return super.deresolve(uri);
