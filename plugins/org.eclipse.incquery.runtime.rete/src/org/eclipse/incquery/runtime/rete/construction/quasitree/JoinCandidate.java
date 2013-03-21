91fd2ec4eb31dc55aa66e22d2798beff8877c926
Adam Dudas <sir.dudas.adam@gmail.com>
2013-03-21 16:17:22 +0100

    [398720] implement Heath-join and add necessary dependencies

--- plugins/org.eclipse.incquery.runtime.rete/src/org/eclipse/incquery/runtime/rete/construction/quasitree/JoinCandidate.java
+++ plugins/org.eclipse.incquery.runtime.rete/src/org/eclipse/incquery/runtime/rete/construction/quasitree/JoinCandidate.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.incquery.runtime.base.api.FunctionalDependencyHelper;
 import org.eclipse.incquery.runtime.rete.collections.CollectionsFactory;
 import org.eclipse.incquery.runtime.rete.construction.Stub;
 import org.eclipse.incquery.runtime.rete.construction.psystem.PConstraint;
@@ -40,7 +41,8 @@ class JoinCandidate<StubHandle> {
 
         varPrimary = getPrimary().getVariablesSet();
         varSecondary = getSecondary().getVariablesSet();
-        varCommon = CollectionsFactory.getSet(varPrimary); varCommon.retainAll(varSecondary);
+        varCommon = CollectionsFactory.getSet(varPrimary);
+        varCommon.retainAll(varSecondary);
     }
 
     /**
@@ -94,21 +96,22 @@ class JoinCandidate<StubHandle> {
     }
 
     private Boolean heath;
+
     // it is a Heath-join iff common variables functionally determine either all primary or all secondary variables
     public boolean isHeath() {
-    	if (heath == null) {
-    		Map<Set<PVariable>, Set<PVariable>> dependencies = new HashMap<Set<PVariable>, Set<PVariable>>(); 		
-    		for (PConstraint pConstraint : primary.getAllEnforcedConstraints()) 
-    			dependencies.putAll(pConstraint.getFunctionalDependencies());	
-    		for (PConstraint pConstraint : secondary.getAllEnforcedConstraints()) 
-    			dependencies.putAll(pConstraint.getFunctionalDependencies());	
-    		
-    		// TODO use functional dependency analysis to determine heathness here
-    		// does varCommon determine either varPrimary or varSecondary?
-    		heath = false;
-    	}
-    	return false;
+        if (heath == null) {
+            Map<Set<PVariable>, Set<PVariable>> dependencies = new HashMap<Set<PVariable>, Set<PVariable>>();
+            for (PConstraint pConstraint : primary.getAllEnforcedConstraints())
+                dependencies.putAll(pConstraint.getFunctionalDependencies());
+            for (PConstraint pConstraint : secondary.getAllEnforcedConstraints())
+                dependencies.putAll(pConstraint.getFunctionalDependencies());
+
+            // does varCommon determine either varPrimary or varSecondary?
+            Set<PVariable> varCommonClosure = FunctionalDependencyHelper.closureOf(varCommon, dependencies);
+
+            heath = varCommonClosure.containsAll(varPrimary) || varCommonClosure.containsAll(varSecondary);
+        }
+        return heath;
     }
-    
-    
+
 }
