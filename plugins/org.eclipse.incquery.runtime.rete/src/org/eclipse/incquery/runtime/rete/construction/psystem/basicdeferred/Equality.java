d8383b7f8f9c4994e5e02485ede58c905a1abc13
bergmann <bergmann@mit.bme.hu>
2013-04-17 16:33:44 +0200

    moot equality PConstraints are less promiscous now during layout

--- plugins/org.eclipse.incquery.runtime.rete/src/org/eclipse/incquery/runtime/rete/construction/psystem/basicdeferred/Equality.java
+++ plugins/org.eclipse.incquery.runtime.rete/src/org/eclipse/incquery/runtime/rete/construction/psystem/basicdeferred/Equality.java
@@ -98,7 +98,7 @@ public class Equality<PatternDescription, StubHandle> extends DeferredPConstrain
 
     @Override
     public boolean isReadyAt(Stub<StubHandle> stub) {
-        return isMoot() || stub.getVariablesIndex().containsKey(who) && stub.getVariablesIndex().containsKey(withWhom);
+        return stub.getVariablesIndex().containsKey(who) && stub.getVariablesIndex().containsKey(withWhom);
         // will be replaced by || if copierNode is available;
         // until then, LayoutHelper.unifyVariablesAlongEqualities(PSystem<PatternDescription, StubHandle, Collector>) is
         // recommended.
