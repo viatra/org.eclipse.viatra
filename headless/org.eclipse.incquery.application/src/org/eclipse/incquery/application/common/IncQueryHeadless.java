6352de0a25e8c7725cdbf2c36ab7cc8037285d79
Istvan Rath <rath@mit.bme.hu>
2013-05-20 13:31:08 +0200

    adjust headless example to API changes, remove unnecessary code

--- headless/org.eclipse.incquery.application/src/org/eclipse/incquery/application/common/IncQueryHeadless.java
+++ headless/org.eclipse.incquery.application/src/org/eclipse/incquery/application/common/IncQueryHeadless.java
@@ -21,7 +21,6 @@ import headless.HeadlessQueries;
 import headless.util.EClassNamesProcessor;
 import headless.util.EObjectProcessor;
 import headless.util.EPackageProcessor;
-import headless.util.EPackageQuerySpecification;
 
 import java.util.Collection;
 
@@ -213,11 +212,6 @@ public class IncQueryHeadless {
 		//     * a "Realm" needs to be set up for headless execution
 		DefaultRealm realm = new DefaultRealm();
 		IObservableSet set = IncQueryObservables.observeMatchesAsSet(matcher);
-//		IObservableSet set;
-//		try {
-//			set = IncQueryObservables.observeMatchesAsSet(EPackageQuerySpecification.instance(),
-//					matcher.getEngine());
-		
 		
 		set.addSetChangeListener(new ISetChangeListener() {
 			@Override
@@ -233,11 +227,6 @@ public class IncQueryHeadless {
 			}
 		});
 		
-		
-//		} catch (IncQueryException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 		/* the same could also be done with a list:
 		IObservableList list = IncQueryObservables.observeMatchesAsList(factory, engine);
 		list.addListChangeListener(new IListChangeListener() {
