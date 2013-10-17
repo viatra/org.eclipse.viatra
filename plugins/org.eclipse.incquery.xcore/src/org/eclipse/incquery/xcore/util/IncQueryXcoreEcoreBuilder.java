4210e10642d90959ac5fa96e92aa0c4b98fd3678
Istvan Rath <rath@mit.bme.hu>
2013-10-17 11:02:20 +0200

    failed attempt to fix weird bugs

--- plugins/org.eclipse.incquery.xcore/src/org/eclipse/incquery/xcore/util/IncQueryXcoreEcoreBuilder.java
+++ plugins/org.eclipse.incquery.xcore/src/org/eclipse/incquery/xcore/util/IncQueryXcoreEcoreBuilder.java
@@ -10,7 +10,15 @@
  *******************************************************************************/
 package org.eclipse.incquery.xcore.util;
 
+import java.io.IOException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EGenericType;
@@ -19,6 +27,8 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.ETypeParameter;
 import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.xcore.XAttribute;
 import org.eclipse.emf.ecore.xcore.XClass;
 import org.eclipse.emf.ecore.xcore.XGenericType;
@@ -27,6 +37,8 @@ import org.eclipse.emf.ecore.xcore.XOperation;
 import org.eclipse.emf.ecore.xcore.XReference;
 import org.eclipse.emf.ecore.xcore.XTypeParameter;
 import org.eclipse.emf.ecore.xcore.util.XcoreEcoreBuilder;
+import org.eclipse.incquery.patternlanguage.emf.eMFPatternLanguage.PatternModel;
+import org.eclipse.incquery.patternlanguage.patternLanguage.Pattern;
 import org.eclipse.incquery.querybasedfeatures.runtime.QueryBasedFeatureSettingDelegateFactory;
 import org.eclipse.incquery.runtime.api.IPatternMatch;
 import org.eclipse.incquery.runtime.api.IQuerySpecification;
@@ -35,6 +47,7 @@ import org.eclipse.incquery.runtime.extensibility.QuerySpecificationRegistry;
 import org.eclipse.incquery.xcore.XIncQueryDerivedFeature;
 import org.eclipse.incquery.xcore.generator.IncQueryXcoreGenerator;
 import org.eclipse.incquery.xcore.mappings.IncQueryXcoreMapper;
+import org.eclipse.xtext.ui.resource.IResourceSetProvider;
 
 import com.google.inject.Inject;
 
@@ -91,6 +104,37 @@ public class IncQueryXcoreEcoreBuilder extends XcoreEcoreBuilder {
         return eStructuralFeature;
     }
     
+    
+    @Inject
+    private IResourceSetProvider resSetProvider;
+    
+        
+    private Pattern createNewResoureSet(Pattern pattern)
+            throws IOException {
+        URI uri = pattern.eResource().getURI();
+        IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
+        IFile file = workspaceRoot.getFile(new Path(uri.toPlatformString(true)));
+        ResourceSet resourceSet = null;
+        IProject project = file.getProject();
+        resourceSet = resSetProvider.get(project);
+        URI fileURI = URI.createPlatformResourceURI(file.getFullPath().toString(), false);
+        Resource resource = resourceSet.createResource(fileURI);
+        resource.load(null);
+        PatternModel topElement = (PatternModel) resource.getContents().get(0);
+        for (Pattern p : topElement.getPatterns()) {
+            if(p.getName().equals(pattern.getName())) {
+                // found pattern
+                return p;
+            }
+        }
+        return null;
+    }
+    
+    /**
+     * As a side effect of this call, the SettingDelegateFactory will be registered to handle things for dynamic resources.
+     * @param eStructuralFeature
+     * @param xIncQueryDerivedFeature
+     */
     protected void handleIncQueryDerivedFeature(final EStructuralFeature eStructuralFeature, final XIncQueryDerivedFeature xIncQueryDerivedFeature) {
         eStructuralFeature.setName(nonNullName(xIncQueryDerivedFeature.getName()));
         handleETypedElement(eStructuralFeature, xIncQueryDerivedFeature);
@@ -103,7 +147,38 @@ public class IncQueryXcoreEcoreBuilder extends XcoreEcoreBuilder {
         runnables.add(new Runnable() {
             @Override
             public void run() {
-                org.eclipse.incquery.patternlanguage.patternLanguage.Pattern pattern = xIncQueryDerivedFeature.getPattern();
+                org.eclipse.incquery.patternlanguage.patternLanguage.Pattern _pattern = xIncQueryDerivedFeature.getPattern();
+                
+                
+                Pattern pattern = null;
+         
+                /* 
+                 * attempt to fix a lot of weird issues:
+                 * instead of using this pattern object, we make another one ala Query Explorer
+                 * 
+                 * doesn't work, because it will get into an infinite loop during scoping-inference
+                 * 
+                 */
+                
+                
+                /*
+                if (_pattern!=null && _pattern.eResource()!=null) {
+                
+	                try {
+	                	pattern = createNewResoureSet(_pattern);
+	                	System.out.println("hack worked");
+	                } catch (Exception e) {
+	                	e.printStackTrace();
+	                }
+                } else {
+                	// fall back to old ways
+                	System.out.println("fallback was necessary");
+                	pattern = _pattern;
+                }
+                */
+                
+                pattern = _pattern;
+                
                 if (pattern != null && pattern.eContainer() != null) {
                     Object factory = EStructuralFeature.Internal.SettingDelegate.Factory.Registry.INSTANCE.get(IncQueryXcoreGenerator.queryBasedFeatureFactory);
                     if (factory != null && factory instanceof QueryBasedFeatureSettingDelegateFactory) {
