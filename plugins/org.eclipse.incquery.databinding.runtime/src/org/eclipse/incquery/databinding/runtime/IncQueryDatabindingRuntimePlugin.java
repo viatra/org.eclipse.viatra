44b6564e8dab74caaa2a239312f600219b459e31
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2014-03-13 16:13:46 +0100

    [430258] Removed unnecessary org.eclipse.ui dependency from data binding
    Change-Id: I73cce46c467c696b12c5a84c4d9594e38cfa0954

--- plugins/org.eclipse.incquery.databinding.runtime/src/org/eclipse/incquery/databinding/runtime/IncQueryDatabindingRuntimePlugin.java
+++ plugins/org.eclipse.incquery.databinding.runtime/src/org/eclipse/incquery/databinding/runtime/IncQueryDatabindingRuntimePlugin.java
@@ -1,31 +1,21 @@
 package org.eclipse.incquery.databinding.runtime;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
-public class IncQueryDatabindingRuntimePlugin extends AbstractUIPlugin {
+public class IncQueryDatabindingRuntimePlugin extends Plugin {
 
     public static final String PLUGIN_ID = "org.eclipse.incquery.databinding.runtime";
     
     // The shared instance
     private static IncQueryDatabindingRuntimePlugin plugin;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-     */
     @Override
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-     */
     @Override
     public void stop(BundleContext context) throws Exception {
         plugin = null;
