4a3e49950ed00fc3bdd7cea01b5c287561c37614
Zoltan Ujhelyi <ujhelyiz@mit.bme.hu>
2014-06-03 19:15:00 +0200

    [406073] Defined additional LS check operations

---
+++ plugins/org.eclipse.incquery.runtime.localsearch/src/org/eclipse/incquery/runtime/localsearch/operations/check/CountCheck.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2013, Zoltan Ujhelyi, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Zoltan Ujhelyi - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.incquery.runtime.localsearch.operations.check;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.incquery.runtime.localsearch.MatchingFrame;
+import org.eclipse.incquery.runtime.localsearch.exceptions.LocalSearchException;
+import org.eclipse.incquery.runtime.localsearch.matcher.ISearchContext;
+import org.eclipse.incquery.runtime.localsearch.matcher.LocalSearchMatcher;
+
+import com.google.common.collect.Iterators;
+
+/**
+ * Calculates the count of matches for a called matcher
+ * 
+ * @author Zoltan Ujhelyi
+ *
+ */
+public class CountCheck extends CheckOperation {
+
+    LocalSearchMatcher calledMatcher;
+    Map<Integer, Integer> frameMapping;
+    private int position;
+
+    public CountCheck(LocalSearchMatcher calledMatcher, Map<Integer, Integer> frameMapping, int position) {
+        super();
+        this.calledMatcher = calledMatcher;
+        this.frameMapping = frameMapping;
+        this.position = position;
+    }
+
+    @Override
+    public void onInitialize(MatchingFrame frame, ISearchContext context) throws LocalSearchException {
+        
+    }
+
+    @Override
+    protected boolean check(MatchingFrame frame) throws LocalSearchException {
+        final MatchingFrame mappedFrame = calledMatcher.editableMatchingFrame();
+        for (Entry<Integer, Integer> entry : frameMapping.entrySet()) {
+            mappedFrame.setValue(entry.getValue(), frame.getValue(entry.getKey()));
+        }
+        int count = calledMatcher.countMatches(mappedFrame);
+        return ((Integer)frame.getValue(position)) == count;
+    }
+
+}
