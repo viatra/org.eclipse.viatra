c14d6ebc3687256f90cff371428a7634d4c82aed
Istvan David <davidi@inf.mit.bme.hu>
2014-08-11 17:18:46 +0200

    Added Eclipse LogAppender to the VEPL UI plugin

--- plugins/org.eclipse.viatra.cep.vepl.ui/src/org/eclipse/viatra/cep/vepl/ui/VeplUiModule.java
+++ plugins/org.eclipse.viatra.cep.vepl.ui/src/org/eclipse/viatra/cep/vepl/ui/VeplUiModule.java
@@ -11,18 +11,47 @@
 
 package org.eclipse.viatra.cep.vepl.ui;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.viatra.cep.vepl.ui.syntaxhighlight.CepDslAntlrTokenToAttributeIdMapper;
 import org.eclipse.viatra.cep.vepl.ui.syntaxhighlight.CepDslHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.contentassist.XtextContentAssistProcessor;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+
+import com.google.inject.Binder;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used within the IDE.
  */
 public class VeplUiModule extends org.eclipse.viatra.cep.vepl.ui.AbstractVeplUiModule {
+    private static final String LOGGER_ROOT = "org.eclipse.viatra.cep";
+
     public VeplUiModule(AbstractUIPlugin plugin) {
         super(plugin);
+    }
+
+    @Provides
+    @Singleton
+    Logger provideLoggerImplementation() {
+        Logger logger = Logger.getLogger(LOGGER_ROOT);
+        logger.setAdditivity(false);
+        logger.addAppender(new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)));
+        logger.addAppender(new EclipseLogAppender());
+        return logger;
+    }
+
+    @Override
+    public void configure(Binder binder) {
+        super.configure(binder);
+        binder.bind(String.class)
+                .annotatedWith(Names.named(XtextContentAssistProcessor.COMPLETION_AUTO_ACTIVATION_CHARS))
+                .toInstance(".,");
     }
 
     public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration() {
