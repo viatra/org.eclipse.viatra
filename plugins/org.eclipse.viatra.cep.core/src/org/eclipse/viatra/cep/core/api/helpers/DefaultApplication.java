549c6e6c0bd543dcb4d66cb4fb15c7d94e009706
Istvan David <davidi@inf.mit.bme.hu>
2015-01-14 23:13:38 +0100

    Created DefaultApplication helper class.
    Change-Id: I32d0adb023dc50054ca2d76e1dca6a16e517bc94
    Signed-off-by: Istvan David <davidi@inf.mit.bme.hu>

---
+++ plugins/org.eclipse.viatra.cep.core/src/org/eclipse/viatra/cep/core/api/helpers/DefaultApplication.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2004-2014, Istvan David, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Istvan David - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.viatra.cep.core.api.helpers;
+
+import org.eclipse.viatra.cep.core.api.engine.CEPEngine;
+import org.eclipse.viatra.cep.core.metamodels.automaton.EventContext;
+import org.eclipse.viatra.cep.core.streams.EventStream;
+
+/**
+ * Helper class for CEP applications. By extending it, a {@link CEPEngine} and an {@link EventStream} is available for
+ * use. The user must implement the {@link DefaultApplication#configureRules()} method by specifying the rules to be
+ * instantiated for the engine.
+ * 
+ * @author Istvan David
+ * 
+ */
+public abstract class DefaultApplication {
+    private CEPEngine engine;
+    private EventStream eventStream;
+
+    public DefaultApplication() {
+        setUpEngine();
+        setUpEventStream();
+        configureRules();
+    }
+
+    protected void setUpEngine() {
+        engine = CEPEngine.newEngine(EventContext.CHRONICLE);
+    }
+
+    protected void setUpEventStream() {
+        eventStream = engine.getStreamManager().newEventStream();
+    }
+
+    protected abstract void configureRules();
+
+    public CEPEngine getEngine() {
+        return engine;
+    }
+
+    public EventStream getEventStream() {
+        return eventStream;
+    }
+}
