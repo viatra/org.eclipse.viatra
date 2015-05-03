9d02930f3e43f30184478f8834504b15eeb2a0ea
Istvan David <davidi@inf.mit.bme.hu>
2015-05-03 22:09:26 +0200

    [461218] Updated integration tests
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Istvan David <davidi@inf.mit.bme.hu>

--- tests/org.eclipse.viatra.cep.tests.integration/src-gen/org/eclipse/viatra/cep/tests/integration/model/patterns/complex/Follows_Pattern.java
+++ tests/org.eclipse.viatra.cep.tests.integration/src-gen/org/eclipse/viatra/cep/tests/integration/model/patterns/complex/Follows_Pattern.java
@@ -1,10 +1,6 @@
 package org.eclipse.viatra.cep.tests.integration.model.patterns.complex;
 
-import com.google.common.collect.Maps;
-import java.util.Map;
-import org.eclipse.viatra.cep.core.api.events.ParameterizableEventInstance;
 import org.eclipse.viatra.cep.core.api.patterns.ParameterizableComplexEventPattern;
-import org.eclipse.viatra.cep.core.metamodels.events.Event;
 import org.eclipse.viatra.cep.core.metamodels.events.EventsFactory;
 import org.eclipse.viatra.cep.tests.integration.model.patterns.atomic.A1_Pattern;
 import org.eclipse.viatra.cep.tests.integration.model.patterns.atomic.A2_Pattern;
@@ -19,19 +15,5 @@ public class Follows_Pattern extends ParameterizableComplexEventPattern {
     addEventPatternRefrence(new A1_Pattern(), 1);
     addEventPatternRefrence(new A2_Pattern(), 1);
     setId("org.eclipse.viatra.cep.tests.integration.model.patterns.complex.follows_pattern");
-  }
-  
-  @Override
-  public boolean evaluateParameterBindings(final Event event) {
-    if(event instanceof ParameterizableEventInstance){
-    	return evaluateParameterBindings((ParameterizableEventInstance) event);
-    }
-    return true;
-  }
-  
-  public boolean evaluateParameterBindings(final ParameterizableEventInstance event) {
-    Map<String, Object> params = Maps.newHashMap();
-    return evaluateParamBinding(params, event);
-    
   }
 }
--- tests/org.eclipse.viatra.cep.tests.integration/src-gen/org/eclipse/viatra/cep/tests/integration/model/patterns/complex/MultiplicityAtLeast_Pattern.java
+++ tests/org.eclipse.viatra.cep.tests.integration/src-gen/org/eclipse/viatra/cep/tests/integration/model/patterns/complex/MultiplicityAtLeast_Pattern.java
@@ -1,10 +1,6 @@
 package org.eclipse.viatra.cep.tests.integration.model.patterns.complex;
 
-import com.google.common.collect.Maps;
-import java.util.Map;
-import org.eclipse.viatra.cep.core.api.events.ParameterizableEventInstance;
 import org.eclipse.viatra.cep.core.api.patterns.ParameterizableComplexEventPattern;
-import org.eclipse.viatra.cep.core.metamodels.events.Event;
 import org.eclipse.viatra.cep.core.metamodels.events.EventsFactory;
 import org.eclipse.viatra.cep.tests.integration.model.patterns.atomic.A2_Pattern;
 import org.eclipse.viatra.cep.tests.integration.model.patterns.complex.anonymous._AnonymousPattern_2;
@@ -19,19 +15,5 @@ public class MultiplicityAtLeast_Pattern extends ParameterizableComplexEventPatt
     addEventPatternRefrence(new _AnonymousPattern_2(), 1);
     addEventPatternRefrence(new A2_Pattern(), 1);
     setId("org.eclipse.viatra.cep.tests.integration.model.patterns.complex.multiplicityatleast_pattern");
-  }
-  
-  @Override
-  public boolean evaluateParameterBindings(final Event event) {
-    if(event instanceof ParameterizableEventInstance){
-    	return evaluateParameterBindings((ParameterizableEventInstance) event);
-    }
-    return true;
-  }
-  
-  public boolean evaluateParameterBindings(final ParameterizableEventInstance event) {
-    Map<String, Object> params = Maps.newHashMap();
-    return evaluateParamBinding(params, event);
-    
   }
 }
