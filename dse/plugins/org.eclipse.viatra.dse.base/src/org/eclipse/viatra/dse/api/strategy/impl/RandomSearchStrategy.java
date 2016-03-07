c88edf2a0848bccc2086d014b47e77881ab6b440
András Szabolcs Nagy <nasz013@gmail.com>
2016-03-07 11:51:12 +0100

    Ported random search strategy

--- dse/plugins/org.eclipse.viatra.dse.base/src/org/eclipse/viatra/dse/api/strategy/impl/RandomSearchStrategy.java
+++ dse/plugins/org.eclipse.viatra.dse.base/src/org/eclipse/viatra/dse/api/strategy/impl/RandomSearchStrategy.java
@@ -1,11 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2010-2014, Miklos Foldenyi, Andras Szabolcs Nagy, Abel Hegedus, Akos Horvath, Zoltan Ujhelyi and Daniel Varro
+ * Copyright (c) 2010-2016, Andras Szabolcs Nagy, Zoltan Ujhelyi and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * Contributors:
- *   Miklos Foldenyi - initial API and implementation
  *   Andras Szabolcs Nagy - initial API and implementation
  *******************************************************************************/
 package org.eclipse.viatra.dse.api.strategy.impl;
@@ -13,10 +12,12 @@ package org.eclipse.viatra.dse.api.strategy.impl;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.apache.log4j.Logger;
 import org.eclipse.viatra.dse.api.DSEException;
-import org.eclipse.viatra.dse.api.strategy.interfaces.LocalSearchStrategyBase;
+import org.eclipse.viatra.dse.api.strategy.interfaces.IStrategy;
 import org.eclipse.viatra.dse.base.DesignSpaceManager;
 import org.eclipse.viatra.dse.base.ExplorerThread;
 import org.eclipse.viatra.dse.base.GlobalContext;
@@ -24,10 +25,11 @@ import org.eclipse.viatra.dse.base.ThreadContext;
 import org.eclipse.viatra.dse.designspace.api.ITransition;
 import org.eclipse.viatra.dse.designspace.api.TrajectoryInfo;
 import org.eclipse.viatra.dse.objectives.Fitness;
+import org.eclipse.viatra.dse.solutionstore.SolutionStore;
 
-public class RandomSearchStrategy extends LocalSearchStrategyBase {
+public class RandomSearchStrategy implements IStrategy {
 
-    private class SharedData {
+    private static class SharedData {
         public final AtomicInteger triesLeft;
         public final int minDepth;
         public final int maxDepth;
@@ -46,8 +48,10 @@ public class RandomSearchStrategy extends LocalSearchStrategyBase {
     private SharedData shared;
     private TrajectoryInfo trajectoryInfo;
     int nth;
-    private boolean isInterrupted = false;
     private ThreadContext context;
+    private AtomicBoolean isInterrupted = new AtomicBoolean(false);
+    private SolutionStore solutionStore;
+    private Logger logger = Logger.getLogger(getClass());
 
     public RandomSearchStrategy(int minDepth, int maxDepth, int numberOfTries) {
         shared = new SharedData(minDepth, maxDepth, numberOfTries);
@@ -57,11 +61,12 @@ public class RandomSearchStrategy extends LocalSearchStrategyBase {
     }
 
     @Override
-    public void init(ThreadContext context) {
+    public void initStrategy(ThreadContext context) {
         this.context = context;
         dsm = context.getDesignSpaceManager();
         trajectoryInfo = dsm.getTrajectoryInfo();
         gc = context.getGlobalContext();
+        solutionStore = gc.getSolutionStore2();
 
         Object sharedObject = gc.getSharedObject();
         if (sharedObject == null) {
@@ -72,69 +77,86 @@ public class RandomSearchStrategy extends LocalSearchStrategyBase {
             shared = (SharedData) sharedObject;
         }
 
-        if (!gc.getSolutionStore().isStrategyDependent()) {
-            throw new DSEException("Random search needs strategy dependent solution store.");
-        }
-
         maxDepth = rnd.nextInt(shared.maxDepth - shared.minDepth) + shared.minDepth;
+
+        logger.info("Initied");
     }
 
     @Override
-    public ITransition getNextTransition(boolean lastWasSuccessful) {
-
-        if (isInterrupted) {
-            return null;
-        }
+    public void explore() {
 
         do {
+
+            boolean globalConstraintsAreSatisfied = context.checkGlobalConstraints();
+            if (!globalConstraintsAreSatisfied) {
+                boolean isSuccessfulUndo = dsm.undoLastTransformation();
+                if (!isSuccessfulUndo) {
+                    logger.info("Global contraint is not satisifed and cannot backtrack.");
+                    break;
+                } else {
+                    logger.debug("Global contraint is not satisifed, backtrack.");
+                    continue;
+                }
+            }
+
+            Fitness fitness = context.calculateFitness();
+            if (fitness.isSatisifiesHardObjectives()) {
+                solutionStore.newSolution(context);
+                boolean isSuccessfulUndo = dsm.undoLastTransformation();
+                if (!isSuccessfulUndo) {
+                    logger.info("Found a solution but cannot backtrack.");
+                    break;
+                } else {
+                    logger.debug("Found a solution, backtrack.");
+                    continue;
+                }
+            }
+
             if (trajectoryInfo.getDepthFromRoot() < maxDepth) {
 
                 Collection<? extends ITransition> transitions = dsm.getTransitionsFromCurrentState();
                 int index = rnd.nextInt(transitions.size());
                 ITransition transition = getByIndex(transitions, index);
-                if (transition.isAssignedToFire()) {
-                    dsm.fireActivation(transition);
-                } else {
-                    return transition;
-                }
+                dsm.fireActivation(transition);
 
             } else {
 
-                context.calculateFitness();
-                gc.getSolutionStore().newSolution(context);
-                if ((nth = shared.triesLeft.getAndDecrement()) > 0) {
+                nth = shared.triesLeft.getAndDecrement();
+                logger.debug(nth + " tries left");
+                if (nth > 0) {
 
                     while (dsm.undoLastTransformation()) {
                     }
-
-                    tryStartNewThread(context);
-
                     maxDepth = rnd.nextInt(shared.maxDepth - shared.minDepth) + shared.minDepth;
 
                 } else {
-                    return null;
+                    break;
                 }
             }
-        } while (gc.getState().equals(GlobalContext.ExplorationProcessState.RUNNING));
 
-        return null;
+            boolean loopInTrajectory = dsm.isCurentStateInTrajectory();
+            if (loopInTrajectory) {
+                boolean isSuccessfulUndo = dsm.undoLastTransformation();
+                if (!isSuccessfulUndo) {
+                    throw new DSEException(
+                            "The new state is present in the trajectoy but cannot bactkrack. Should never happen!");
+                } else {
+                    logger.info("The new state is already visited in the trajectory, backtrack.");
+                }
+            }
+
+        } while (isInterrupted.get());
+
+        logger.info("Terminated.");
+    }
+
+    @Override
+    public void interruptStrategy() {
+        isInterrupted.set(true);
     }
 
     private ExplorerThread tryStartNewThread(ThreadContext context) {
         return gc.tryStartNewThread(context, context.getModelRoot(), true, new RandomSearchStrategy());
-    }
-
-    @Override
-    public void newStateIsProcessed(boolean isAlreadyTraversed, Fitness fitness,
-            boolean constraintsNotSatisfied) {
-        if (constraintsNotSatisfied) {
-            dsm.undoLastTransformation();
-        }
-    }
-
-    @Override
-    public void interrupted() {
-        isInterrupted = true;
     }
 
     private static ITransition getByIndex(Collection<? extends ITransition> availableTransitions, int index) {
@@ -150,5 +172,4 @@ public class RandomSearchStrategy extends LocalSearchStrategyBase {
         }
         throw new IndexOutOfBoundsException("size: " + availableTransitions.size() + ", index: " + index);
     }
-
 }
