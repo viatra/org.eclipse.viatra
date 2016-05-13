ea544a4ed3437c1d9ade10b5215f0dac21a0efcb
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2016-05-13 10:53:18 +0200

    [493602] Fixed an observable leak in ObservableLabelFeature
    Change-Id: I58a07c746825e18dc1160d86ef82730d66564073
    Signed-off-by: Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>

--- addon/plugins/org.eclipse.viatra.addon.databinding.runtime/src/org/eclipse/viatra/addon/databinding/runtime/observables/ObservableLabelFeature.java
+++ addon/plugins/org.eclipse.viatra.addon.databinding.runtime/src/org/eclipse/viatra/addon/databinding/runtime/observables/ObservableLabelFeature.java
@@ -10,11 +10,18 @@
  *******************************************************************************/
 package org.eclipse.viatra.addon.databinding.runtime.observables;
 
-import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.viatra.addon.databinding.runtime.api.ViatraObservables;
 import org.eclipse.viatra.query.runtime.api.IPatternMatch;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
 
 /**
  * An observable label feature is a computed label, that can refer to the various parameters of the match, and reacts to
@@ -23,18 +30,53 @@ import org.eclipse.viatra.query.runtime.api.IPatternMatch;
  * @author Zoltan Ujhelyi
  * 
  */
+@SuppressWarnings("rawtypes")
 public class ObservableLabelFeature extends ComputedValue {
     IPatternMatch match;
     String expression;
     Object container;
+    final Map<String, IObservableValue> expressionMap;
+    private List<String> expressionTokens;
+    
 
     public ObservableLabelFeature(IPatternMatch match, String expression, Object container) {
         super(String.class);
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(expression), "Expression must be set for label feature");
         this.match = match;
         this.expression = expression;
         this.container = container;
+        expressionMap = initializeObservableMap(expression, match);
     }
 
+    private final Map<String, IObservableValue> initializeObservableMap(String expression, IPatternMatch match) {
+        Map<String, IObservableValue> map = new HashMap<>();
+
+        //StringTokenizer tokenizer = new StringTokenizer(expression, "$", true);
+        Splitter tokenizer = Splitter.on("$");
+        expressionTokens = tokenizer.splitToList(expression);
+        boolean inExpression = false;
+        boolean foundToken = false;
+        for (String token : expressionTokens) {
+            if (Strings.isNullOrEmpty(token)) {
+                if (inExpression && !foundToken) {
+                    throw new IllegalArgumentException("Empty reference ($$) in message is not allowed.");
+                }
+                inExpression = !inExpression;
+            } else if (inExpression) {
+                if (!map.containsKey(token)) {
+                    IObservableValue value = ViatraObservables.getObservableValue(match, token);
+                    map.put(token, value);
+                }
+                foundToken = true;
+            }
+        }
+        if (inExpression) {
+            throw new IllegalArgumentException("Inconsistent model references - a $ character is missing.");
+        }
+
+        return map;
+    }
+    
     public Object getContainer() {
         return container;
     }
@@ -52,35 +94,31 @@ public class ObservableLabelFeature extends ComputedValue {
 
         StringBuilder sb = new StringBuilder();
         try {
-
-            StringTokenizer tokenizer = new StringTokenizer(expression, "$", true);
-            if (expression.isEmpty() || tokenizer.countTokens() == 0) {
-                throw new IllegalArgumentException("Expression must not be empty.");
-            }
-            boolean inExpression = false;
-            boolean foundToken = false;
-            while (tokenizer.hasMoreTokens()) {
-                String token = tokenizer.nextToken();
-                if (token.equals("$")) {
-                    if (inExpression && !foundToken) {
-                        throw new IllegalArgumentException("Empty reference ($$) in message is not allowed.");
-                    }
-                    inExpression = !inExpression;
-                } else if (inExpression) {
-                    IObservableValue value = ViatraObservables.getObservableValue(match, token);
+            for (int i = 0; i < expressionTokens.size(); i++) {
+                String token = expressionTokens.get(i);
+                if ((i % 2) == 0) {
+                    Preconditions.checkState(expressionMap.containsKey(token), "Error while parsing expression " + token);
+                    IObservableValue value = expressionMap.get(token);
                     sb.append(value.getValue());
-                    foundToken = true;
                 } else {
                     sb.append(token);
                 }
             }
-            if (inExpression) {
-                throw new IllegalArgumentException("Inconsistent model references - a $ character is missing.");
-            }
 
-        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             return "ERROR: " + e.getMessage();
         }
         return sb.toString();
     }
+
+    @Override
+    public synchronized void dispose() {
+        for (Entry<String, IObservableValue> entry : expressionMap.entrySet()) {
+            entry.getValue().dispose();
+        }
+        expressionMap.clear();
+        super.dispose();
+    }
+    
+    
 }
