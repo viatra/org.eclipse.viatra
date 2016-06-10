7c1c478a24f5c87ee1647eb2b8a31efff9e82347
András Szabolcs Nagy <nasz013@gmail.com>
2016-06-10 10:40:06 +0200

    [495593] missing copyright header

--- dse/plugins/org.eclipse.viatra.dse.base/src/org/eclipse/viatra/dse/base/SingletonSetConflictResolver.java
+++ dse/plugins/org.eclipse.viatra.dse.base/src/org/eclipse/viatra/dse/base/SingletonSetConflictResolver.java
@@ -1,8 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2016, Andras Szabolcs Nagy, Zoltan Ujhelyi and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *   Andras Szabolcs Nagy - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.viatra.dse.base;
 
 import org.eclipse.viatra.transformation.evm.api.resolver.ChangeableConflictSet;
 import org.eclipse.viatra.transformation.evm.api.resolver.ConflictResolver;
 
+/**
+ * 
+ * @author Andras Szabolcs Nagy
+ *
+ */
 public class SingletonSetConflictResolver implements ConflictResolver {
 
     protected ChangeableConflictSet conflictSet;
@@ -15,5 +29,5 @@ public class SingletonSetConflictResolver implements ConflictResolver {
     public ChangeableConflictSet createConflictSet() {
         return conflictSet;
     }
-    
+
 }
