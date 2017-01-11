352e6ee59a33ee77b123f22b2b189e6317ccfacf
András Szabolcs Nagy <nasz013@gmail.com>
2017-01-11 17:29:39 +0100

    [510272] Batch activation coder until the incremental is repaired

--- dse/plugins/org.eclipse.viatra.dse/src/org/eclipse/viatra/dse/base/DseActivationNotificationListener.java
+++ dse/plugins/org.eclipse.viatra.dse/src/org/eclipse/viatra/dse/base/DseActivationNotificationListener.java
@@ -9,24 +9,20 @@
  *******************************************************************************/
 package org.eclipse.viatra.dse.base;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.eclipse.viatra.dse.api.DSEException;
 import org.eclipse.viatra.dse.statecode.IStateCoder;
 import org.eclipse.viatra.query.runtime.api.IPatternMatch;
 import org.eclipse.viatra.transformation.evm.api.Activation;
 import org.eclipse.viatra.transformation.evm.api.Agenda;
 import org.eclipse.viatra.transformation.evm.api.event.ActivationState;
 import org.eclipse.viatra.transformation.evm.api.event.EventType;
+import org.eclipse.viatra.transformation.evm.api.resolver.ChangeableConflictSet;
 import org.eclipse.viatra.transformation.evm.notification.IActivationNotificationListener;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
 /**
- * This class is responsible for maintaining an activation - activation code bidirectional map for maintaining
- * activation codes incrementally.
+ * This class is responsible for maintaining an <i>activation - activation code</i> bidirectional map incrementally.
  * 
  * @author Andras Szabolcs Nagy
  *
@@ -37,11 +33,14 @@ public class DseActivationNotificationListener implements IActivationNotificatio
     protected BiMap<Activation<?>, Object> activationIds;
     protected IStateCoder stateCoder;
 
-    protected Set<Activation<?>> newActivations = new HashSet<>();
-    protected Set<Activation<?>> removedActivations = new HashSet<>();
+//    protected Set<Activation<?>> newActivations = new HashSet<>();
+//    protected Set<Activation<?>> removedActivations = new HashSet<>();
+    private ChangeableConflictSet conflictSet;
+//    private Logger logger = Logger.getLogger(getClass());
     
     public DseActivationNotificationListener(Agenda agenda, IStateCoder stateCoder) {
         defaultActivationListener = agenda.getActivationListener();
+        conflictSet = agenda.getConflictSet();
         activationIds = HashBiMap.create();
         this.stateCoder = stateCoder;
     }
@@ -53,24 +52,49 @@ public class DseActivationNotificationListener implements IActivationNotificatio
     @Override
     public void activationRemoved(Activation<?> activation, ActivationState oldState) {
         defaultActivationListener.activationRemoved(activation, oldState);
-        newActivations.remove(activation);
-        removedActivations.add(activation);
+//*
+//        removedActivations.add(activation);
+//        newActivations.remove(activation);
+/*/
+        if(!removedActivations.add(activation)) {
+            logger.debug("Abnormal: already marked to remove: " + activation);
+        } else {
+            logger.debug("marked to remove: " + activation);
+        }
+        if(newActivations.remove(activation)) {
+            logger.debug("Abnormal: removed from new activations: " + activation);
+        }
+//*/
     }
 
     @Override
     public void activationCreated(Activation<?> activation, ActivationState inactiveState) {
         defaultActivationListener.activationCreated(activation, inactiveState);
-        newActivations.add(activation);
-        removedActivations.remove(activation);
+//*
+//        newActivations.add(activation);
+//        removedActivations.remove(activation);
+/*/
+        if (activation.isEnabled()) {
+            if (!newActivations.add(activation)) {
+                logger.debug("Abnormal: already added as new: " + activation);
+            } else {
+                logger.debug("activation added: " + activation);
+            }
+        }
+        if(removedActivations.remove(activation)) {
+            logger.debug("Abnormal: was already marked to remove: " + activation);
+        }
+//*/
     }
 
     @Override
     public void activationChanged(Activation<?> activation, ActivationState oldState, EventType event) {
         defaultActivationListener.activationChanged(activation, oldState, event);
-        if (event.equals(EventType.RuleEngineEventType.FIRE)) {
-            return;
-        }
-        throw new DSEException("Unhandled case of activation change. If this happens, it is a bug.");
+//        if (activation.getState().isInactive()) {
+//            removedActivations.add(activation);
+//            newActivations.remove(activation);
+////            logger.debug("Removed as became inactive: " + activation);
+//        }
     }
 
     public Object getActivationId(Activation<?> activation) {
@@ -86,18 +110,40 @@ public class DseActivationNotificationListener implements IActivationNotificatio
     }
     
     public void updateActivationCodes() {
-        for (Activation<?> activation : removedActivations) {
-            activationIds.remove(activation);
+//        logger.debug("Updating activation codes.");
+
+        activationIds.clear();
+        for (Activation<?> activation : conflictSet.getConflictingActivations()) {
+            Object activationCode = createActivationCode(activation);
+            activationIds.forcePut(activation, activationCode);
         }
-        for (Activation<?> activation : newActivations) {
-            Object activationId = createActivationCode(activation);
-            if (activationIds.containsValue(activationId)) {
-                System.out.println("Same activation id.");
-                return;
-            }
-            activationIds.put(activation, activationId);
-        }
-        removedActivations.clear();
-        newActivations.clear();
+
+//        for (Activation<?> activation : removedActivations) {
+//            activationIds.remove(activation);
+////            logger.debug("removed activation: " + activationId);
+//        }
+//
+//        for (Activation<?> activation : newActivations) {
+//            if (activation.getState().isInactive()) {
+//                continue;
+//            }
+//            Object activationId = createActivationCode(activation);
+//            activationIds.forcePut(activation, activationId);
+////            logger.debug("new activation: " + activationId);
+////            Activation<?> similarActivation = activationIds.inverse().get(activationId);
+////            if (similarActivation != null) {
+////                logger.debug("Activation " + toStringAct(activation) + " is already present with id: " + activationId);
+////                if (similarActivation.isEnabled()) {
+////                    logger.warn("Duplicate activation code: " + activationId);
+////                } else {
+////                    logger.debug("Force put: " + activationId);
+////                }
+////                continue;
+////            }
+////            activationIds.put(activation, activationId);
+//        }
+//        removedActivations.clear();
+//        newActivations.clear();
     }
+    
 }
