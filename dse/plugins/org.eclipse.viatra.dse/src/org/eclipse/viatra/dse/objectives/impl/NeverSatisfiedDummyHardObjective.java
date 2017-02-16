7c100028bba6a6adda378b0441c67c379694503b
András Szabolcs Nagy <nasz013@gmail.com>
2017-02-16 15:42:26 +0100

    [512276] NeverSatisfiedDummyHardObjective

---
+++ dse/plugins/org.eclipse.viatra.dse/src/org/eclipse/viatra/dse/objectives/impl/NeverSatisfiedDummyHardObjective.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2017, Andras Szabolcs Nagy and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ *   Andras Szabolcs Nagy - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.viatra.dse.objectives.impl;
+
+import org.eclipse.viatra.dse.base.ThreadContext;
+import org.eclipse.viatra.dse.objectives.IObjective;
+
+/**
+ * This hard objective is never fulfilled. Use it if all states should be regarded as an invalid solution.
+ * 
+ * @author Andras Szabolcs Nagy
+ *
+ */
+public class NeverSatisfiedDummyHardObjective extends BaseObjective {
+
+    private static final String DEFAULT_NAME = "NeverSatisfiedDummyHardObjective";
+
+    public NeverSatisfiedDummyHardObjective() {
+        super(DEFAULT_NAME);
+    }
+
+    public NeverSatisfiedDummyHardObjective(String name) {
+        super(name);
+    }
+
+    @Override
+    public Double getFitness(ThreadContext context) {
+        return 0d;
+    }
+
+    @Override
+    public boolean isHardObjective() {
+        return true;
+    }
+
+    @Override
+    public boolean satisifiesHardObjective(Double fitness) {
+        return false;
+    }
+
+    @Override
+    public IObjective createNew() {
+        return this;
+    }
+
+}
