ab1a4ab4b3c520acdeee699775c211517ec48bf8
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2017-07-05 15:08:20 +0200

    [510568] Do not create classpath entries ending with a slash character
    Change-Id: I447a7bcf72b592c452c9deda610fe988331ec67c
    Signed-off-by: Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
    (cherry picked from commit 531e69f7104178bfc89c8bc7c052cce145469a03)

--- query/plugins/org.eclipse.viatra.query.tooling.core/src/org/eclipse/viatra/query/tooling/core/project/ViatraQueryNature.java
+++ query/plugins/org.eclipse.viatra.query.tooling.core/src/org/eclipse/viatra/query/tooling/core/project/ViatraQueryNature.java
@@ -29,8 +29,8 @@ public class ViatraQueryNature implements IProjectNature {
     public static final String NATURE_ID = "org.eclipse.viatra.query.projectnature"; //$NON-NLS-1$
     public static final String XTEXT_NATURE_ID = XtextProjectHelper.NATURE_ID;
     public static final String BUILDER_ID = "org.eclipse.viatra.query.tooling.ui.projectbuilder";//$NON-NLS-1$
-    public static final String SRCGEN_DIR = "src-gen/"; //$NON-NLS-1$
-    public static final String SRC_DIR = "src/"; //$NON-NLS-1$
+    public static final String SRCGEN_DIR = "src-gen"; //$NON-NLS-1$
+    public static final String SRC_DIR = "src"; //$NON-NLS-1$
     public static final String EXECUTION_ENVIRONMENT = "JavaSE-1.7"; // $NON_NLS-1$
     public static final String VQGENMODEL = "generator.vqgen";
 
