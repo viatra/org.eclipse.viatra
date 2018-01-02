25734e8201fc172fff539cbe41994555a4cd24f2
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2018-01-02 19:28:31 +0100

    [527371] Ensure generated projects require Java 8

--- query/plugins/org.eclipse.viatra.query.tooling.core/src/org/eclipse/viatra/query/tooling/core/project/ViatraQueryNature.java
+++ query/plugins/org.eclipse.viatra.query.tooling.core/src/org/eclipse/viatra/query/tooling/core/project/ViatraQueryNature.java
@@ -31,7 +31,7 @@ public class ViatraQueryNature implements IProjectNature {
     public static final String BUILDER_ID = "org.eclipse.viatra.query.tooling.ui.projectbuilder";//$NON-NLS-1$
     public static final String SRCGEN_DIR = "src-gen/"; //$NON-NLS-1$
     public static final String SRC_DIR = "src/"; //$NON-NLS-1$
-    public static final String EXECUTION_ENVIRONMENT = "JavaSE-1.7"; // $NON_NLS-1$
+    public static final String EXECUTION_ENVIRONMENT = "JavaSE-1.8"; // $NON_NLS-1$
     public static final String VQGENMODEL = "generator.vqgen";
 
     private IProject project;
