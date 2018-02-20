0de3ce13b0eaa4ee5d496ab1a7628de9c3ab3740
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2018-02-20 17:42:35 +0100

    [508544] Enhanced hover documentation of metamodel types
    Change-Id: Ic27d74d842f27dc23fc490b602dc9ae894ec6fa2
    Signed-off-by: Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>

--- query/plugins/org.eclipse.viatra.query.patternlanguage.emf.ui/src/org/eclipse/viatra/query/patternlanguage/emf/ui/labeling/EMFPatternLanguageHoverDocumentationProvider.java
+++ query/plugins/org.eclipse.viatra.query.patternlanguage.emf.ui/src/org/eclipse/viatra/query/patternlanguage/emf/ui/labeling/EMFPatternLanguageHoverDocumentationProvider.java
@@ -11,10 +11,12 @@
 package org.eclipse.viatra.query.patternlanguage.emf.ui.labeling;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.eclipse.emf.codegen.ecore.genmodel.GenPackage;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EModelElement;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
@@ -27,6 +29,7 @@ import org.eclipse.viatra.query.patternlanguage.emf.vql.Annotation;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.AnnotationParameter;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.ClassType;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.Pattern;
+import org.eclipse.viatra.query.patternlanguage.emf.vql.PatternLanguagePackage;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.ReferenceType;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.Variable;
 import org.eclipse.viatra.query.patternlanguage.emf.vql.VariableReference;
@@ -37,6 +40,7 @@ import org.eclipse.viatra.query.tooling.core.generator.genmodel.IVQGenmodelProvi
 import org.eclipse.xtext.common.types.JvmTypeReference;
 import org.eclipse.xtext.xbase.ui.hover.XbaseHoverDocumentationProvider;
 
+import com.google.common.base.Optional;
 import com.google.inject.Inject;
 
 /**
@@ -139,26 +143,33 @@ public class EMFPatternLanguageHoverDocumentationProvider extends XbaseHoverDocu
     }
     
     private void appendStructuralFeatureHover(StringBuilder sb, EStructuralFeature feature) {
-        sb.append("<p><strong>Defined in:</strong> " + feature.getEContainingClass().getName() + "</p>");
-        sb.append("<p><strong>Takes value of:</strong> " + feature.getEType().getName() + "</p>");
-        if (feature.isMany())
-            sb.append("<p><strong>Multiplicity:</strong> <em>to-many</em></p>");
-        else
-            sb.append("<p><strong>Multiplicity:</strong> <em>to-one</em></p>");
-        sb.append("<p><strong>Derived:</strong> " + feature.isDerived() + "</p>");
-        sb.append("<p><strong>Transient:</strong> " + feature.isTransient() + "</p>");
+        sb.append(getGenmodelDocumentation(feature));
+        sb.append("<dl>");
+        sb.append("<dt>Defined in</dt>");
+        sb.append("<dd>" + feature.getEContainingClass().getName() + "</dd>");
+        sb.append("<dt>Takes value of</dt>");
+        sb.append("<dd>"+feature.getEType().getName() + "</dd>");
+        sb.append("<dt>Multiplicity</dt>");
+        sb.append(feature.isMany() ? "<dd><em>to-many</em></dd>" : "<dd><em>to-one</em></dd>");
+        sb.append("<dt>Derived</dt>");
+        sb.append("<dd>" + feature.isDerived() + "</dd>");
+        sb.append("<dt>Transient</dt> ");
+        sb.append("<dd>" + feature.isTransient() + "</dd>");
         if (feature instanceof EReference) {
             EReference reference = (EReference) feature;
-            sb.append("<p><strong>Containment:</strong> " + reference.isContainment() + "</p>");
-            sb.append("<p><strong>Container:</strong> " + reference.isContainer() + "</p>");
+            sb.append("<dt>Containment</dt>");
+            sb.append("<dd>" + reference.isContainment() + "</dt>");
+            sb.append("<dt>Container</dt> ");
+            sb.append("<dd>" + reference.isContainer() + "</dd>");
             EReference eOpposite = reference.getEOpposite();
-            if (eOpposite != null) {
-                sb.append("<p><strong>EOpposite:</strong> " + eOpposite.getName() + "</p>");
-            }
+            sb.append("<dt>Opposite</dt>");
+            sb.append(eOpposite == null ? "<dd>No opposite defined</dd>" : "<dd>" + eOpposite.getName() + "</dd>"); 
         }
+        sb.append("</dl>");
     }
 
     private void appendEClassifierHover(StringBuilder sb, EClassifier classifier) {
+        sb.append(getGenmodelDocumentation(classifier));
         if (classifier instanceof EClass) {
             EClass eClass = (EClass) classifier;
             List<EClass> eAllSuperTypes = eClass.getEAllSuperTypes();
@@ -192,6 +203,14 @@ public class EMFPatternLanguageHoverDocumentationProvider extends XbaseHoverDocu
         return String.format("<b>EMF Type</b>: %s<br /><b>Java Type</b>: %s", emfTypeString, javaTypeString);
     }
 
+    private String getGenmodelDocumentation(EModelElement classifier) {
+        return Optional.fromNullable(classifier.getEAnnotation("http://www.eclipse.org/emf/2002/GenModel"))
+                .transform(ann -> ann.getDetails())
+                .transform(details -> details.get("documentation"))
+                .transform(doc -> "<p>" + doc + "</p>")
+                .or("");
+    }
+    
     private String getTypeString(EClassifier emfType) {
         String emfTypeString;
         final String packageUri = emfType.getEPackage() != null ? "(<i>" + emfType.getEPackage().getNsURI() + "</i>)" : "";
@@ -201,7 +220,7 @@ public class EMFPatternLanguageHoverDocumentationProvider extends XbaseHoverDocu
 
     @Override
     protected String getDerivedElementInformation(EObject o) {
-        return (o instanceof Pattern) ? null : super.getDerivedElementInformation(o);
+        return (Objects.equals(PatternLanguagePackage.eNS_URI, o.eClass().getEPackage().getNsURI())) ? null : super.getDerivedElementInformation(o);
     }
 
 }
